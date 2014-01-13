fe1b704c7eccab42f6a07acc8caa9411f223e080
Scott Vossen <scottvossen@gmail.com>
2014-01-13 16:24:08 -0600

    map mismatched fields

--- site/js/models/book-model.js
+++ site/js/models/book-model.js
@@ -1,11 +1,21 @@
 var app = app || {};
 
 app.Book = Backbone.Model.extend({
+
+	// idAttribute: '_id',
+	
     defaults: {
         coverImage: 'img/placeholder.png',
         title: 'No title',
         author: 'Unknown',
         releaseDate: 'Unknown',
         keywords: 'None'
-    }
+    }, 
+
+    parse: function( response ) {
+
+    	// map mismatched attributes in responses
+	    response.id = response._id;
+	    return response;
+	}
 });
