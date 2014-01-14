527f5f4704aafd6991448b4342084b3f11836bd0
Scott Vossen <scottvossen@gmail.com>
2014-01-14 09:16:41 -0600

    fix persistence bug with dates

--- site/js/models/book-model.js
+++ site/js/models/book-model.js
@@ -3,12 +3,12 @@ var app = app || {};
 app.Book = Backbone.Model.extend({
 
 	// idAttribute: '_id',
-	
+
     defaults: {
         coverImage: 'img/placeholder.png',
         title: 'No title',
         author: 'Unknown',
-        releaseDate: 'Unknown',
+        releaseDate: undefined,
         keywords: 'None'
     }, 
 
